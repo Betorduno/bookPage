f9959ceb4fe9fdd2fb6c9a2543d2889b70c0aa90
hard75 <barrazajesusarmando@gmail.com>
2019-12-21 23:30:02 -0500

    endpoints file

---
+++ .nuxt/router.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _2cf755f6 = () => interopDefault(import('../pages/completed.vue' /* webpackChunkName: "pages/completed" */))
+const _4b630e2d = () => interopDefault(import('../pages/download.vue' /* webpackChunkName: "pages/download" */))
+const _22795bd2 = () => interopDefault(import('../pages/nuxt.config.js' /* webpackChunkName: "pages/nuxt.config" */))
+const _8d1da306 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/completed",
+    component: _2cf755f6,
+    name: "completed"
+  }, {
+    path: "/download",
+    component: _4b630e2d,
+    name: "download"
+  }, {
+    path: "/nuxt.config",
+    component: _22795bd2,
+    name: "nuxt.config"
+  }, {
+    path: "/",
+    component: _8d1da306,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter () {
+  return new Router(routerOptions)
+}
---
+++ utils/endspoint.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'http://localhost:1337'
+
+export {
+    BASE_URL
+}
